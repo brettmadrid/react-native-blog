f94ad54f0e24731d0116cbbaea89639d13f5f2d1
brettmadrid <brettmadrid9@gmail.com>
2020-09-25 15:06:37 -0700

    added trash icon and styling. Added id to blog obj

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,19 +1,28 @@
-import React, { useState } from 'react'
+import createDataContext from './createDataContext'
 
-const BlogContext = React.createContext()
-
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([])
-
-  const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: `BlogPost #${blogPosts.length + 1}` }])
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ]
+    default:
+      return state
   }
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  )
 }
 
-export default BlogContext
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: 'add_blogpost' })
+  }
+}
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+)
