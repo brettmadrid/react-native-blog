8b00e326c3208d0859f5305de00378c5fbea8919
brettmadrid <brettmadrid9@gmail.com>
2020-09-25 17:54:24 -0700

    completed ShowScreen to show single blogpost

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import { Context } from '../context/BlogContext'
 
-const ShowScreen = props => {
-  console.log('props', props)
+const ShowScreen = ({ navigation }) => {
+  // get state
+  const { state } = useContext(Context)
+  // get id param sent through navigation.getParam object
+  const id = navigation.getParam('id')
+
+  const blogPost = state.find(blogPost => blogPost.id === id)
+
   return (
     <View>
-      <Text>Show Screen</Text>
+      <Text>{blogPost.title}</Text>
     </View>
   )
 }
