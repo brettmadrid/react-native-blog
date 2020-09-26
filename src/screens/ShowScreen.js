25d9eb5bb0abdeae22a730714ff8e540848ae68c
brettmadrid <brettmadrid9@gmail.com>
2020-09-25 17:37:58 -0700

    created ShowScreen component

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const ShowScreen = props => {
+  console.log('props', props)
+  return (
+    <View>
+      <Text>Show Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default ShowScreen
